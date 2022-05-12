911bbc35857bc1874bbc3705918da34be5333032
Thainara <thai.cds1@gmail.com>
2022-05-12 13:42:42 -0400

    Netflix clone

---
+++ js/owl/setup.js
@@ -0,0 +1,57 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function botaoAssistirFilmePrincipal(){
+    window.open("https://youtu.be/KPLWWIOCOOQ");
+}
+
+function botaoMaisInfo(){
+    window.open("https://www.themoviedb.org/tv/1399-game-of-thrones?language=pt-BR");
+}
+
+function theWitch() {
+    window.open("https://www.youtube.com/watch?v=OtLG_mWdZZg&ab_channel=NetflixBrasil")
+    
+}
+
+function dexter(){
+    window.open("https://www.youtube.com/watch?v=l9H1uSS_zkk&t=1s&ab_channel=SHOWTIME")
+}
+
+function pijamaListrado(){
+    window.open("https://www.youtube.com/watch?v=uZD-ySKl91A&ab_channel=PlayS%C3%A9ries")
+}
+
+function kimenoyaiba(){
+    window.open("https://www.youtube.com/watch?v=t6MXHczeEqc&ab_channel=CrunchyrollCollection")
+}
+
+function oRouxinol(){
+    window.open("https://www.youtube.com/watch?v=OfBv9C_Ie8c&ab_channel=RapidTrailer")
+}
+
+function deVoltaHogwarts(){
+    window.open("https://www.youtube.com/watch?v=FhJx6bNBlTY&ab_channel=HBOMaxBrasil")
+}
+
+function oPianista(){
+    window.open("https://www.youtube.com/watch?v=BFwGqLa_oAo&ab_channel=MovieclipsClassicTrailers")
+}
+
+function matrix(){
+    window.open("https://www.youtube.com/watch?v=hdJ8RT3HVe8&ab_channel=Ingresso.com")
+}
